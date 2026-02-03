638a2bd47ac4308339ae025f7f9d835885e96e9f
Tótok Patrik <totokpatrik@gmail.com>
2026-02-03 16:08:24 +0100

    Initial commit

---
+++ src/main/java/com/totok/springecom/SpringEcomApplication.java
@@ -0,0 +1,13 @@
+package com.totok.springecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEcomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEcomApplication.class, args);
+	}
+
+}
